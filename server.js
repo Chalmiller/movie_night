09f5c8a91e5420c8e7417fe6e10569f9aeb1d68b
Chalmiller <chalmiller1@gmail.com>
2020-02-27 10:58:48 -0800

    Deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -22,6 +23,15 @@ const usersRouter = require('./backend/routes/movie_user');
 
 app.use('/movies', moviesRouter);
 app.use('/users', usersRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', "build", "index.html"));
+    });
+} 
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
