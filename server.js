62ba25269608f15b573d21c972b3d2d0126df97a
Chalmiller <chalmiller1@gmail.com>
2020-02-27 11:53:07 -0800

    Change port

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+
+// Local dev
+//  const port = process.env.PORT || 5000;
+
+// Heroku deployment
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
