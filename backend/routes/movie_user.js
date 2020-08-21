7ee0cf3e2f1f31f30dc014f557e5bfde1a476993
Chalmiller <chalmiller1@gmail.com>
2020-08-21 14:20:24 -0700

    comments

--- backend/routes/movie_user.js
+++ backend/routes/movie_user.js
@@ -7,6 +7,7 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Add route
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const newUser = new User({username});
